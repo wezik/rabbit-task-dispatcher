1d0f2600535c081f2ff8bbdb77eb3a0466493bc4
Dominik <wezik@users.noreply.github.com>
2024-03-16 14:22:49 +0100

    Go-worker - Consumer implementation

--- go-worker/go.mod
+++ go-worker/go.mod
@@ -1,3 +1,8 @@
 module go-worker
 
 go 1.21.6
+
+require (
+        github.com/streadway/amqp v1.0.0
+        github.com/gofiber/fiber/v2 v2.52.1
+)
--- go-worker/main.go
+++ go-worker/main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"github.com/streadway/amqp"
+)
 
 func main() {
-        fmt.Println("Hello, World")
+
+        amqpServerURL := "amqp://guest:guest@localhost:5672/test"
+        rmqConnection, err := amqp.Dial(amqpServerURL)
+        if err != nil {
+                panic(err)
+        }
+        defer rmqConnection.Close()
+
+        rmqChannel, err := rmqConnection.Channel()
+        if err != nil {
+                panic(err)
+        }
+        defer rmqChannel.Close()
+
+        messages, err := rmqChannel.Consume(
+                "task-dispatcher",
+                "",
+                true,
+                false,
+                false,
+                false,
+                nil,
+        )
+        if err != nil {
+                log.Println(err)
+        }
+
+        log.Println("Successfully connected to RabbitMQ")
+        log.Println("Waiting for messages")
+
+        forever := make(chan bool)
+
+        go func() {
+                for message := range messages {
+                        log.Printf(" > Received messages: %s\n", message.Body)
+                }
+        }()
+
+        <-forever
 }
