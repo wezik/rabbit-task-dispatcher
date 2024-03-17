b227316422a1c4943f8f2b37fddbc34172b68a72
Dominik <wezik@users.noreply.github.com>
2024-03-17 18:15:04 +0100

    Worker - added mockup publishing in response

--- go-worker/main.go
+++ go-worker/main.go
@@ -41,8 +41,55 @@ func main() {
         go func() {
                 for message := range messages {
                         log.Printf(" > Received messages: %s\n", message.Body)
+                        log.Printf(" > Sending back resolved")
+                        publish(message.Body)
                 }
         }()
 
         <-forever
+}
+
+func publish(body []byte) {
+        amqpServerURL := "amqp://guest:guest@localhost:5672/test"
+        rmqConnection, err := amqp.Dial(amqpServerURL)
+        if err != nil {
+                panic(err)
+        }
+        defer rmqConnection.Close()
+
+        rmqChannel, err := rmqConnection.Channel()
+        if err != nil {
+                panic(err)
+        }
+
+        defer rmqChannel.Close()
+
+	_, err = rmqChannel.QueueDeclare(
+		"task-response", // queue name
+		true,            // durable
+		false,           // auto delete
+		false,           // exclusive
+		false,           // no wait
+		nil,             // arguments
+	)
+	if err != nil {
+		panic(err)
+	}
+
+        message := amqp.Publishing{
+                ContentType: "text/plain",
+                Body: body,
+        }
+
+        err = rmqChannel.Publish(
+                "",
+                "task-response",
+                false,
+                false,
+                message,
+        ); 
+
+        if err != nil {
+                panic(err)
+        }
 }
